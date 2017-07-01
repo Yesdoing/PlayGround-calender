013dbdd397aa7ca66af40e440ecbde88d4076c44
yesdoing <fkrmtleo12@gmail.com>
2017-07-01 15:58:02 +0900

    method 추출

--- src/ShowCalendar.java
+++ src/ShowCalendar.java
@@ -2,28 +2,37 @@ import java.util.Scanner;
 
 public class ShowCalendar {
 	public static final int[] MONTH = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int result = 0;
-		
-		while(true) {
-			System.out.println("달을 입력하세요.");
-			result = scan.nextInt();
-			if(result == -1) break;
-			printCalendar(result);
+	
+	public static void main(String[] args) {			
+		while(inputMonth()) { 
 			System.out.println();
 		}
+	}
+		
+	private static boolean inputMonth() {
+		Scanner scan = new Scanner(System.in);	
+		
+		System.out.println("달을 입력하세요.");
+		int result = scan.nextInt();
+		if(result == -1) return false;
+		else printCalendar(result);
+		
+		return true;
+
 	}
 	
 	private static void printCalendar(int result) {
 		System.out.println("Hello Calendar!");
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("--------------------");
-		for(int i = 1; i<=MONTH[result]; i++) {
-			if(i < 10) System.out.print(" " + i + " ");
-			else System.out.print(i + " ");
-			
-			if(i % 7 == 0) System.out.println();
+		for(int i = 1; i<=MONTH[result-1]; i++) {
+			printDay(i);
 		}
+	}
+	
+	private static void printDay(int day) {
+		if(day < 10) System.out.print(" " + day + " ");
+		else System.out.print(day + " ");
+		if(day % 7 == 0) System.out.println();
 	}
 }
